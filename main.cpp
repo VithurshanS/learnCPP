d59d019165c31229148d1b7da906f6884fc5a2b6
VithurshanS <vithurshansivanathan610@gmail.com>
2026-06-23 13:59:22 +0530

    created vector class based on base template

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include "mem_leak.h"
 #include "variable_initialization.h"
 #include "const_initialization.h"
-
+#include "myvector.h"
 int main(){
 	//int result = add(4,7);
 	//std::cout << "the result is "<< result << std::endl;	
@@ -12,5 +12,12 @@ int main(){
 	//perform_leak();
 	variable_init();
 	//const_init();
+	auto v = myvector<int>();
+	v.add(4);
+	v.add(10);
+	v.add(10);
+	v.print();
+	//std::cout<<"v.data is " << v.retdata() << std::endl;
+	v.getcompletememorymap();
 	return 0;
 }
